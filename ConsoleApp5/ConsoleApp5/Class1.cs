54cf6e8d59ff32c6cb5f631672492b2707e436d3
Maria Hlushkova <90055466+mashokk@users.noreply.github.com>
2021-10-30 10:29:22 +0300

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class Class1
+    {
+        class test
+        {
+            static void Main2(string[] args)
+            {
+                int param = 10;
+                testMethod(ref param); //Выйдет значение = 11
+                Console.WriteLine("Значение переменной param в методе Main = {0}", param); //Выйдет значение = 11
+                Console.ReadLine();
+            }
+            static void testMethod(ref int param)
+            {
+                param++;
+                Console.WriteLine("Значение переменной param в методе testMethod = {0}", param);
+            }
+            static void testMethod2(out int param)
+            {
+                param = 10;
+                param++;
+                Console.WriteLine("Значение переменной param в методе testMethod2 = {0}", param);
+            }
+        }
+    }
+}
