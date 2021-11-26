62ceff34618126069b10660adf2ad8aa7e4aff04
Mash Ok <90055466+mashokk@users.noreply.github.com>
2021-11-26 12:45:40 +0300

    Add files via upload

---
+++ OOP_Lab5/OOP_Lab5/Class1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOP_Lab5
+{
+        public abstract class Pastry //АБСТРАКТНЫЙ КЛАСС
+        {
+            public int Consignment; //партия товара
+            public int Month; //месяц выпуска 1-12
+
+            protected Pastry() { }
+
+            protected Pastry (int consignment, int month)
+            {
+                Consignment = consignment;
+                Month = month;
+            }
+        /*
+        При наследовании нередко возникает необходимость изменить в классе-наследнике
+        функционал метода, который был унаследован от базового класса. В этом случае
+        класс-наследник может переопределять методы и свойства базового класса.
+        
+        Те методы и свойства, которые мы хотим сделать доступными для переопределения,
+        в базовом классе помечается модификатором virtual. Такие методы и свойства называют виртуальными.
+
+        А чтобы переопределить метод в классе-наследнике, этот метод определяется с модификатором override.
+        Переопределенный метод в классе-наследнике должен иметь тот же набор параметров, что и виртуальный
+        метод в базовом классе.
+        */
+        public override string ToString() //ИСПОЛЬЗОВАНИЕ ВИРТУАЛЬНЫХ МЕТОДОВ
+            {
+                return "Партия сладостей номер " + Consignment + ", выпущенная в " + Month + " месяце 2021 года.";
+            }
+            public abstract bool DoClone(); //ОДНОИМЕННЫЙ МЕТОД!!
+        }
+
+        /*class UserClass : BaseClone, ICloneable //ЧЕ ОТ МЕНЯ ХОТЯТ
+        {
+            ....
+        }*/
+    class Sweet : Pastry
+    {
+        public override bool DoClone()
+        {
+            return true;
+        }
+        public int Gram;
+        public Sweet() { }
+        public Sweet(int Gram, int Consignment, int Month)
+        {
+            this.Gram = Gram;
+            this.Consignment = Consignment;
+            this.Month = Month;
+        }
+        public override string ToString() //Во всех классах (иерархии) переопределить метод ToString(), который выводит информацию о типе объекта и его текущих значениях.
+        {
+            return "Партия конфет номер " + Consignment + ", выпущенная в " + Month + " месяце содержит " + Gram + " грамм.";
+        }
+    }
+
+    class Cookie : Pastry
+    {
+        public override bool DoClone()
+        {
+            return true;
+        }
+        public int Diameter;
+        public Cookie() { }
+        public Cookie(int Diameter, int Consignment, int Month)
+        {
+            this.Diameter = Diameter;
+            this.Consignment = Consignment;
+            this.Month = Month;
+        }
+        public override string ToString()
+        {
+            return "Партия печенюшек диаметром " + Diameter + " см номер " + Consignment + ", выпущена в " + Month + " месяце.";
+        }
+    }
+    class Caramel : Sweet
+    {
+        public override bool DoClone()
+        {
+            return true;
+        }
+        public string Color;
+        public Caramel() { }
+        public Caramel(int Gram, int Consignment, int Month, string Color)
+        {
+            this.Gram = Gram;
+            this.Consignment = Consignment;
+            this.Month = Month;
+            this.Color = Color; //красного, синего...
+        }
+        public override string ToString()
+        {
+            return "Партия конфет " + Color + " цвета номер " + Consignment + ", выпущенная в " + Month + " месяце содержит " + Gram + " грамм.";
+        }
+    }
+    class ChocoSweet : Sweet
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Вы съели конфету");
+        }
+        public override bool DoClone()
+        {
+            return true;
+        }
+        public string Chocolat;
+        public ChocoSweet() { }
+        public ChocoSweet(int Gram, int Consignment, int Month, string Chocolat)
+        {
+            this.Gram = Gram;
+            this.Consignment = Consignment;
+            this.Month = Month;
+            this.Chocolat = Chocolat; //горького, молочного...
+        }
+        public override string ToString()
+        {
+            return "Партия конфет из " + Chocolat + " шоколада номер " + Consignment + ", выпущенная в " + Month + " месяце содержит " + Gram + " грамм.";
+        }
+    }
+    // partial
+    sealed class Box : ChocoSweet //Сделайте один из классов sealed; ТРЕТЬЕ ЗАДАНИЕ
+    {
+        public override bool DoClone()
+        {
+            return false;
+        }
+        public int Number;
+        public Box() { }
+        public Box(int Number, int Gram, int Consignment, int Month, string Chocolat)
+        {
+            this.Number = Number;
+            this.Gram = Gram;
+            this.Consignment = Consignment;
+            this.Month = Month;
+            this.Chocolat = Chocolat;
+        }
+        public override string ToString()
+        {
+            return "Партия конфет из " + Chocolat + " шоколада номер " + Consignment + ", выпущенная в " + Month + " месяце содержит " + Gram + " грамм и упаковывается в коробки по " + Number + " конфет.";
+        }
+    }
+    interface IClonable //ИНТЕРФЕЙС 
+    {
+        bool DoClone(); //ОДНОИМЕННЫЙ МЕТОД??
+    }
+
+    /*Формальным параметром метода должна быть ссылка на абстрактный класс или наиболее
+    общий интерфейс в вашей иерархии классов.В методе iIAmPrinting
+    определите тип объекта и вызовите ToString(). В демонстрационной
+    программе создайте массив, содержащий ссылки на разнотипные объекты
+    ваших классов по иерархии, а также объект класса Printer и последовательно
+    вызовите его метод IAmPrinting со всеми ссылками в качестве аргументов.*/
+    class Printer //дополнительный класс Printer c полиморфным методом IAmPrinting(SomeAbstractClassorInterface someobj).
+    {
+        public string IAmPrinting(Pastry someobj)
+        {
+            return someobj.ToString();
+        }
+        public string IAmPrinting(Sweet someobj)
+        {
+            return someobj.ToString();
+        }
+        public string IAmPrinting(Cookie someobj)
+        {
+            return someobj.ToString();
+        }
+    }
+}
---
+++ OOP_Lab5/OOP_Lab5/Program.cs
@@ -0,0 +1,65 @@
+﻿//Кондитерское изделие, Конфета, Карамель, Шоколадная конфета,
+//Печенюшка, Коробка конфет.
+using System;
+using System.Collections.Generic;
+
+namespace OOP_Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pastry pastry = new Pastry(); //кондитерское изделие
+            Sweet sweet = new Sweet(1000, 837362, 6); //int Gram, int Consignment, int Month
+            Caramel caramel = new Caramel(1500, 374564, 7, "красного"); //int Gram, int Consignment, int Month, string Color
+            ChocoSweet chocosweet = new ChocoSweet(2000, 792955, 8, "горького"); //int Gram, int Consignment, int Month, string Chocolat
+            Cookie cookie = new Cookie(12, 385762, 9); //Diameter, int Consignment, int Month
+            Box box = new Box(16, 5000, 100247, 10, "молочного"); //int Number, int Gram, int Consignment, int Month, string Chocolat
+
+            //Console.WriteLine(pastry.ToString());
+            Console.WriteLine(sweet.ToString());
+            Console.WriteLine(caramel.ToString());
+            Console.WriteLine(chocosweet.ToString());
+            chocosweet.Eat();
+            Console.WriteLine(cookie.ToString());
+            Console.WriteLine(box.ToString());
+
+            List<Pastry> list = new List<Pastry> { new Sweet(1000, 837362, 6), new Cookie(12, 385762, 9) };
+
+            Printer printer = new Printer();
+            foreach (Pastry v in list)
+            {
+                Console.WriteLine(printer.IAmPrinting(v));
+            }
+
+            /*Composition composition = new Composition();
+
+            composition.cp.Add(caramel);
+            composition.cp.Add(chocosweet);
+            composition.cp.Add(box);
+            composition.GetPastry("sweet");
+            composition.GetTransport("2"); // не пашет*/
+        }
+
+        /*public partial class Composition
+        {
+            public List<object> cp = new System.Collections.Generic.List<object>();
+            public void GetPastry(string pastr)
+            {
+                foreach (Pastry v in cp) { }
+            }
+        }
+        public void GetTransport(int num)
+        {
+            string buf;
+            for (int i = 0; i < cp.Count; i++) // ругается на cp
+            {
+                buf = Convert.ToString(cp[i]); // ругается на cp
+                if (buf.Contains(Convert.ToString(num)))
+                {
+                    Console.WriteLine(buf);
+                }
+            }
+        }*/
+    }
+}
